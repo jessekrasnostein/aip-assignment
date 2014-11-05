3794b0eb401a5150268bc75300430d379373a66d
James Gosbell <james.gosbell@servicerocket.com>
2014-11-05 19:19:23 +1100

    Commented on ShoppingListController

--- Shoppaholic-war/src/java/au/edu/uts/aip/shoppaholic/web/ShoppingListController.java
+++ Shoppaholic-war/src/java/au/edu/uts/aip/shoppaholic/web/ShoppingListController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package au.edu.uts.aip.shoppaholic.web;
 
 
@@ -30,8 +25,6 @@ import javax.validation.ConstraintViolationException;
 /**
  * ShoppingListController is responsible for managing all functionality around
  shopping items and lists.
- *
- * @author jessekras
  */
 @Named
 @SessionScoped
